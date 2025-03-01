7b800825cf64db44ea8dcf6be00ce35d8679d280
Nathan <natefintak@gmail.com>
2025-03-01 15:55:18 -0500

    in progress

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class WordGuess {
 	public static void main(String[] args) {
 		java.util.Random randomGenerator = new java.util.Random();
-		int numOfGuesses = 4;
 		String[] wordList = {"cat", "pie", "fork", "book"};
 		int randWord = randomGenerator.nextInt(wordList.length);
 		String keyword = wordList[randWord];
+		int numOfGuesses = 4;
 
+		System.out.println("Let's Play Wordguess version 1.0");
+		while (numOfGuesses > 0) {
+			System.out.println("Current Guesses: ");
+			for (int i = 0; i <= keyword.length(); i++) {
+				System.out.print("_");
+			}
+
+			numOfGuesses--;
+		}
 	}
 }
