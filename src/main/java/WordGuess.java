825f2ccf5e36a71edd76ec2baf160ed1249adeb9
Nathan <natefintak@gmail.com>
2025-03-01 00:15:50 -0500

    in progress

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -1,1 +1,25 @@
 //Nathan
+import java.util.Scanner;
+
+/** to do list
+ * make list of short words, max 4 or 5 characters
+ * make input prompt for character
+ * make method that compares input character to single character for proof of concept
+ * make while loop to contain game
+ * make for loop to contain input prompt, info on guesses, and correct characters
+ * make if statement in for loop that uses comparison method
+ *
+ */
+public class WordGuess {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); //make scanner
+		int numOfGuesses = 4;
+
+		System.out.println("Let's Play Wordguess version 1.0");
+		System.out.println("Current Guesses: " + numOfGuesses);
+
+
+	}
+
+	public static char
+}
