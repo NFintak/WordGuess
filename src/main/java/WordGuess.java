f60646ac64c3ee7c81f2b686b53e45b43845ee39
Nathan <natefintak@gmail.com>
2025-03-01 15:43:00 -0500

    started over

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -3,21 +3,11 @@ import java.util.Scanner;
 
 public class WordGuess {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in); //make scanner
-		String keyword = "pie"; //need to convert letters to chars
-		int startingNum = 4;
+		java.util.Random randomGenerator = new java.util.Random();
+		int numOfGuesses = 4;
+		String[] wordList = {"cat", "pie", "fork", "book"};
+		int randWord = randomGenerator.nextInt(wordList.length);
+		String keyword = wordList[randWord];
 
-		System.out.println("Let's Play Wordguess version 1.0"); //starting message
-
-		while (startingNum > 0) {
-			System.out.println("Current Guesses: ");
-			System.out.println("Enter a single character: ");
-			String guess = scan.nextLine(); //need to change to char
-			for (int i = 0; i <= (keyword.length() - 1); i++) {
-				//check guess against chars in keyword
-			}
-			startingNum--;
-			System.out.println("You have " + startingNum + "tries left.");
-		}
 	}
 }
