108b562cb4d80c69da7eec4ad1bbd81a7831cebb
Nathan <natefintak@gmail.com>
2025-03-01 01:36:52 -0500

    in progress

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 public class WordGuess {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); //make scanner
-		String keyWord = "pie";
+		String keyword = "pie"; //need to convert letters to chars
 		int startingNum = 4;
 
-		System.out.println("Let's Play Wordguess version 1.0");
-		System.out.println("Current Guesses: " + startingNum);
-		System.out.println("Enter a single character: ");
-		String guess = scan.nextLine();
+		System.out.println("Let's Play Wordguess version 1.0"); //starting message
 
-	}
-	public static char userGuess(char letter) {
-
+		while (startingNum > 0) {
+			System.out.println("Current Guesses: ");
+			System.out.println("Enter a single character: ");
+			String guess = scan.nextLine(); //need to change to char
+			for (int i = 0; i <= (keyword.length() - 1); i++) {
+				//check guess against chars in keyword
+			}
+			startingNum--;
+			System.out.println("You have " + startingNum + "tries left.");
+		}
 	}
 }
