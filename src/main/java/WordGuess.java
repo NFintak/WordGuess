b1f1f6d3c13f0af76f83b1988e22202728cffdcb
Nathan <natefintak@gmail.com>
2025-03-01 21:24:08 -0500

    moved program to correct file

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -1,40 +1,8 @@
 //Nathan
-import java.util.Scanner;
 
 public class WordGuess {
 
 	public static void main(String[] args) {
-		java.util.Random randomGenerator = new java.util.Random();
-		String[] wordList = {"cat", "pie", "fork", "book"};
-		int randWord = randomGenerator.nextInt(wordList.length);
-		String keyword = wordList[randWord];
-		int numOfGuesses = 4;
-
-		System.out.println("Let's Play Wordguess version 1.0");
-
-		while (numOfGuesses > 0) {
-			Scanner scan = new Scanner(System.in);
-			String hiddenWord = "";
-			for (int i = 0; i <= (keyword.length() - 1); i++) {
-				hiddenWord = keyword.replace(keyword.charAt(i), '*');
-			}
-			System.out.println("Current Guesses: ");
-
-			System.out.println(hiddenWord);
-			System.out.println();
-			System.out.println("You have " + numOfGuesses + " tries left.");
-			System.out.println("Enter a single character: ");
-			String prompt = scan.nextLine();
-			char letter = prompt.charAt(0);
-
-			/*
-			for (int i = 0; i <= (keyword.length() - 1); i++) {
-				if (letter != (keyword.charAt(i))) {
-					System.out.println("");
-				}
-			}
-			*/
-			numOfGuesses--;
-		}
-	}
+		WordGuess game = new WordGuess();
+		game.runGame();
 }
