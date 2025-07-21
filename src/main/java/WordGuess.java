e393105bef7ad4360087541577f8ddff9a44c79e
NFintak <natefintak@gmail.com>
2025-07-21 16:01:19 -0400

    checkpoint

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -12,11 +12,13 @@ public class WordGuess {
 	String[] wordList = {"cat", "pie", "fork", "tree"}; //leave this as an array
 	int randomNum = randomGenerator.nextInt(wordList.length);
 	String keyword = wordList[randomNum];
-	char[] userGuesses = new char[keyword.length()]; //change to arraylist
+	//char[] userGuesses = new char[keyword.length()]; //change to arraylist
+	ArrayList<Character> userGuesses;
 	boolean wordGuessed;
 	int guessesLeft = keyword.length();
 	char letter;
-	char[] keyArray = keyword.toCharArray(); //change to arraylist
+	//char[] keyArray = keyword.toCharArray(); //change to arraylist
+	ArrayList<Character> keyArray = new ArrayList<>();
 
 	//keyword = wordList[randomNum];
 	//userGuesses = new char[keyword.length()];
@@ -32,13 +34,19 @@ public class WordGuess {
 		//playAgain = true;
 	}
 
-	public char[] initializeGameState() { //sets char array for secret word and guesses
+	public ArrayList<Character> initializeGameState() { //sets char array for secret word and guesses
+//		keyword = wordList[randomNum];
+//		userGuesses = new char[this.keyword.length()];
+//		guessesLeft = this.keyword.length();
+//		keyArray = this.keyword.toCharArray();
+//		for (int i = 0; i < this.keyword.length(); i++) {
+//			userGuesses[i] = '_';
+//		}
+//		return userGuesses;
 		keyword = wordList[randomNum];
-		userGuesses = new char[this.keyword.length()];
-		guessesLeft = this.keyword.length();
-		keyArray = this.keyword.toCharArray();
-		for (int i = 0; i < this.keyword.length(); i++) {
-			userGuesses[i] = '_';
+		guessesLeft = keyword.length();
+		for (int i = 0; i < keyword.length(); i++) {
+			userGuesses.add('_');
 		}
 		return userGuesses;
 	}
@@ -48,72 +56,75 @@ public class WordGuess {
 		String prompt = scan.nextLine();
 		letter = prompt.charAt(0);
 		return letter;
+		//return 'z';
 	}
 
-	public char[] printArray(/*char[] userGuesses*/) { //formats user guesses with spaces
+	public void printArray() { //formats user guesses with spaces
 		for (int i = 0; i < this.keyword.length(); i++) {
-			System.out.print(userGuesses[i] + " ");
+			System.out.print(userGuesses.get(i) + " ");
 		}
-		return userGuesses;
 	}
 
 	public int printCurrentState() { //use printArray to show correct player guesses
-		System.out.println("Current Guesses: ");
-		if (!wordGuessed && guessesLeft > 0) {
-			this.printArray(/*userGuesses*/);
-		}
-		System.out.println("\nYou have " + guessesLeft + " tries left");
-		return guessesLeft;
+//		System.out.println("Current Guesses: ");
+//		if (!wordGuessed && guessesLeft > 0) {
+//			this.printArray(/*userGuesses*/);
+//		}
+//		System.out.println("\nYou have " + guessesLeft + " tries left");
+//		return guessesLeft;
+		return 0;
 	}
 
 	public char[] process() { //cycles thru word array and compares against input guess, replaces _ if correct
-		for (int i = 0; i < keyword.length(); i++) {
-			if (letter == keyArray[i] && userGuesses[i] == '_') {
-				userGuesses[i] = letter;
-			} // check if letter in keyword has been guessed
-			//use setCharAt() for this part?
-		}
-		if (guessesLeft == 0 && !(Arrays.equals(userGuesses, keyArray))) {
-			wordGuessed = false;
-		} else if (Arrays.equals(keyArray, userGuesses) && guessesLeft > 0) {
-			wordGuessed = true;
-		}
-		guessesLeft--;
-		return userGuesses;
+//		for (int i = 0; i < keyword.length(); i++) {
+//			if (letter == keyArray[i] && userGuesses[i] == '_') {
+//				userGuesses[i] = letter;
+//			} // check if letter in keyword has been guessed
+//			//use setCharAt() for this part?
+//		}
+//		if (guessesLeft == 0 && !(Arrays.equals(userGuesses, keyArray))) {
+//			wordGuessed = false;
+//		} else if (Arrays.equals(keyArray, userGuesses) && guessesLeft > 0) {
+//			wordGuessed = true;
+//		}
+//		guessesLeft--;
+//		return userGuesses;
+		return null;
 	}
 
 	public void isWordGuessed() { //return boolean, if true display win message
-		if (wordGuessed && guessesLeft >= 0) {
-			playerWon();
-		} else if (!wordGuessed && guessesLeft == 0) {
-			playerLost();
-		}
+//		if (wordGuessed && guessesLeft >= 0) {
+//			playerWon();
+//		} else if (!wordGuessed && guessesLeft == 0) {
+//			playerLost();
+//		}
 	}
 
 	public void /*boolean*/ playerWon() { //display win message
-		System.out.println("**** ****");
-		System.out.println(userGuesses);
-		System.out.println("Congratulations, You Won!");
+//		System.out.println("**** ****");
+//		System.out.println(userGuesses);
+//		System.out.println("Congratulations, You Won!");
 		//return wordGuessed = false;
 	}
 
 	public void /*boolean*/ playerLost() { //display lose message
-		System.out.println(":'( :'( :'(");
-		System.out.println(userGuesses);
-		System.out.println("You Lost! You ran out of guesses.");
+//		System.out.println(":'( :'( :'(");
+//		System.out.println(userGuesses);
+//		System.out.println("You Lost! You ran out of guesses.");
 		//return wordGuessed = true;
 	}
 
 	public boolean askToPlayAgain() { //return boolean, if false leave while loop
-		System.out.println("Would you like to play again? (yes/no)");
-		String tryAgain = scan.nextLine();
-		if (tryAgain.equalsIgnoreCase("no")) {
-			return playAgain = false;
-		} else if (tryAgain.equalsIgnoreCase("yes")){
-			return playAgain = true;
-		} else {
-			return playAgain = false;
-		}
+//		System.out.println("Would you like to play again? (yes/no)");
+//		String tryAgain = scan.nextLine();
+//		if (tryAgain.equalsIgnoreCase("no")) {
+//			return playAgain = false;
+//		} else if (tryAgain.equalsIgnoreCase("yes")){
+//			return playAgain = true;
+//		} else {
+//			return playAgain = false;
+//		}
+		return false;
 	}
 
 	public void gameOver() {
