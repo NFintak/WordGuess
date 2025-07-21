9c772cb08cf93f390f2e307af7b62e1a301a3290
NFintak <natefintak@gmail.com>
2025-07-21 18:22:55 -0400

    checkpoint

--- src/main/java/WordGuess.java
+++ src/main/java/WordGuess.java
@@ -11,11 +11,11 @@ public class WordGuess {
 	boolean playAgain;
 	String[] wordList = {"cat", "pie", "fork", "tree"}; //leave this as an array
 	int randomNum = randomGenerator.nextInt(wordList.length);
-	String keyword = wordList[randomNum];
+	String keyword;
 	//char[] userGuesses = new char[keyword.length()]; //change to arraylist
 	ArrayList<Character> userGuesses;
 	boolean wordGuessed;
-	int guessesLeft = keyword.length();
+	int guessesLeft;
 	char letter;
 	//char[] keyArray = keyword.toCharArray(); //change to arraylist
 	ArrayList<Character> keyArray = new ArrayList<>();
@@ -65,71 +65,70 @@ public class WordGuess {
 		}
 	}
 
-	public int printCurrentState() { //use printArray to show correct player guesses
-//		System.out.println("Current Guesses: ");
-//		if (!wordGuessed && guessesLeft > 0) {
-//			this.printArray(/*userGuesses*/);
-//		}
-//		System.out.println("\nYou have " + guessesLeft + " tries left");
-//		return guessesLeft;
-		return 0;
+	public void printCurrentState() { //use printArray to show correct player guesses
+		System.out.println("Current Guesses: ");
+		if (!wordGuessed && guessesLeft > 0) {
+			this.printArray();
+		}
+		System.out.println("\nYou have " + guessesLeft + " tries left");
+
 	}
 
-	public char[] process() { //cycles thru word array and compares against input guess, replaces _ if correct
-//		for (int i = 0; i < keyword.length(); i++) {
-//			if (letter == keyArray[i] && userGuesses[i] == '_') {
-//				userGuesses[i] = letter;
-//			} // check if letter in keyword has been guessed
-//			//use setCharAt() for this part?
-//		}
-//		if (guessesLeft == 0 && !(Arrays.equals(userGuesses, keyArray))) {
-//			wordGuessed = false;
-//		} else if (Arrays.equals(keyArray, userGuesses) && guessesLeft > 0) {
-//			wordGuessed = true;
-//		}
-//		guessesLeft--;
-//		return userGuesses;
-		return null;
+	public ArrayList<Character> process() { //cycles thru word array and compares against input guess, replaces _ if correct
+		for (int i = 0; i < keyword.length(); i++) {
+			if (letter == keyArray.get(i) && userGuesses.get(i) == '_') {
+				userGuesses.set(i, letter);
+			} // check if letter in keyword has been guessed
+			//use setCharAt() for this part?
+		}
+		if (guessesLeft == 0 && !(Arrays.equals(userGuesses.toArray(), keyArray.toArray()))) {
+			wordGuessed = false;
+		} else {
+			wordGuessed = true;
+		}
+		guessesLeft--;
+		return userGuesses;
 	}
 
 	public void isWordGuessed() { //return boolean, if true display win message
-//		if (wordGuessed && guessesLeft >= 0) {
-//			playerWon();
-//		} else if (!wordGuessed && guessesLeft == 0) {
-//			playerLost();
-//		}
+		if (wordGuessed && guessesLeft >= 0) {
+			playerWon();
+		} else if (!wordGuessed && guessesLeft == 0) {
+			playerLost();
+		}
 	}
 
 	public void /*boolean*/ playerWon() { //display win message
-//		System.out.println("**** ****");
-//		System.out.println(userGuesses);
-//		System.out.println("Congratulations, You Won!");
+		System.out.println("**** ****");
+		System.out.println(userGuesses);
+		System.out.println("Congratulations, You Won!");
 		//return wordGuessed = false;
 	}
 
 	public void /*boolean*/ playerLost() { //display lose message
-//		System.out.println(":'( :'( :'(");
-//		System.out.println(userGuesses);
-//		System.out.println("You Lost! You ran out of guesses.");
+		System.out.println(":'( :'( :'(");
+		System.out.println(userGuesses);
+		System.out.println("You Lost! You ran out of guesses.");
 		//return wordGuessed = true;
 	}
 
 	public boolean askToPlayAgain() { //return boolean, if false leave while loop
-//		System.out.println("Would you like to play again? (yes/no)");
-//		String tryAgain = scan.nextLine();
-//		if (tryAgain.equalsIgnoreCase("no")) {
-//			return playAgain = false;
-//		} else if (tryAgain.equalsIgnoreCase("yes")){
-//			return playAgain = true;
-//		} else {
-//			return playAgain = false;
-//		}
-		return false;
+		System.out.println("Would you like to play again? (yes/no)");
+		String tryAgain = scan.nextLine();
+		if (tryAgain.equalsIgnoreCase("no")) {
+			return playAgain = false;
+		} else if (tryAgain.equalsIgnoreCase("yes")){
+			keyArray.clear();
+			userGuesses.clear();
+			return playAgain = true;
+		} else {
+			return playAgain = false;
+		}
+		//return false;
 	}
 
 	public void gameOver() {
 		System.out.println("Game over.");
-
 	}
 
 	public void runGame() { //pulls the methods written to run the game
